e8b84877b1905194e4bfae1df6c87da2a48eab95
Fatih Gozenc <fatihgozenc@gmail.com>
2022-04-23 14:36:09 +0200

    fix: yarn local script works correct with express

--- scripts/local.js
+++ scripts/local.js
@@ -1,9 +1,11 @@
 const open = require('open');
+const path = require("path");
 const { spawn } = require("child_process");
-const servor = require('servor');
 const EventEmitter = require('events');
+const express = require("express");
 
-const LOCAL_BUILD_URL = "http://localhost:3001/index.html";
+const PORT = 3001;
+const LOCAL_BUILD_URL = `http://localhost:${PORT}`;
 const startTime = Date.now();
 
 const args = process.argv.slice(2);
@@ -12,22 +14,22 @@ const Builder = spawn(process.argv[0], [__dirname + "/build.js", ...args], {
     detached: true,
     stdio: 'inherit'
 });
+
 const Events = new EventEmitter();
 Builder.on("exit", () => Events.emit("build_end"));
 
 Events.on('build_end', () => {
     const builtTime = Date.now();
     console.log(`Built in in ${(builtTime - startTime) / 1000}ms.`);
-    console.log(`Starting server on http://localhost:3001`);
-    servor({
-        root: './build',
-        fallback: './build/index.html',
-        module: false,
-        static: true,
-        credentials: null,
-        port: 3001,
+    console.log(`Starting server on ${LOCAL_BUILD_URL}`);
+
+    const app = express();
+    app.use(express.static("build"));
+    app.get('*', (req, res) => res.sendFile(path.resolve('build', 'index.html')));
+    app.listen(PORT, () => {
+        Events.emit('server_ready');
     });
-    Events.emit('server_ready');
+
 });
 
 Events.on("server_ready", () => {
