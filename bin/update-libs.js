56d1e5e80aa4b8577a32ef8370f2125da7c82c62
Fatih <fatihgozenc@gmail.com>
2023-10-09 00:07:20 +0200

    Updated libraries

--- bin/update-libs.js
+++ bin/update-libs.js
@@ -1,40 +1,41 @@
 const package = require("../package.json");
-const { exec } = require('child_process');
+const { exec } = require("child_process");
 
 const ignoredLibs = ["chalk", "pretty-bytes"];
 
 const deps = package.dependencies
-    ? Object.keys(package.dependencies).filter(e => !ignoredLibs.includes(e)).join(" ")
-    : null;
+  ? Object.keys(package.dependencies)
+      .filter((e) => !ignoredLibs.includes(e))
+      .join(" ")
+  : null;
 const ddeps = package.devDependencies
-    ? Object.keys(package.devDependencies).filter(e => !ignoredLibs.includes(e)).join(" ")
-    : null;
+  ? Object.keys(package.devDependencies)
+      .filter((e) => !ignoredLibs.includes(e))
+      .join(" ")
+  : null;
 
 execute(`npm remove ${ddeps}`)
-    .then(() => {
-        console.log("Deleted ddeps.");
-        return execute(`npm remove ${deps}`);
-    })
-    .then(() => {
-        console.log("Deleted deps.");
-        if (!ddeps) {
-            process.exit(0);
-        }
-        return execute(`npm i ${deps}`);
-    })
-    .then(() => {
-        console.log("Re-Installed deps.");
-        return execute(`npm i -D ${ddeps}`);
-    })
-    .then(() => {
-        console.log("Re-Installed ddeps.");
-    });
+  .then(() => {
+    console.log("Deleted ddeps.");
+    return execute(`npm remove ${deps}`);
+  })
+  .then(() => {
+    console.log("Deleted deps.");
+    return execute(`npm i ${deps}`);
+  })
+  .then(() => {
+    console.log("Re-Installed deps.");
+    return execute(`npm i -D ${ddeps}`);
+  })
+  .then(() => {
+    console.log("Re-Installed ddeps.");
+  });
 
 async function execute(command) {
-    return new Promise((resolve, reject) => {
-        exec(command, (error, stdout) => {
-            if (error) reject(error);
-            resolve(stdout.trim());
-        });
+  return new Promise((resolve, reject) => {
+    exec(command, (error, stdout) => {
+      if (error) reject(error);
+      resolve(stdout.trim());
     });
+  });
 }
