1f47fa1220eca5c667ecea764da6e6c82252e12e
Fatih <fatihgozenc@gmail.com>
2023-11-15 22:59:19 +0100

    0.1.11 - Added parent tsconfig inclusion

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -5,123 +5,149 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const Dotenv = require("dotenv-webpack");
 const { WebpackManifestPlugin } = require("webpack-manifest-plugin");
 const InterpolateHtmlPlugin = require("@gozenc/interpolate-html-plugin");
-const { existsSync } = require("fs");
+const { existsSync, readFileSync } = require("fs");
 
 const appPath = path.resolve("src", "index.tsx");
+const tsConfigPath = path.resolve("tsconfig.json");
 const preloadPath = path.resolve("src", "preload.js");
 
+const tsConfig = existsSync(tsConfigPath)
+  ? JSON.parse(removeTrailingComma(readFileSync(tsConfigPath, "utf8")))
+  : undefined;
+
+function removeTrailingComma(jsonString) {
+  const fixedString = jsonString.replace(/,\s*([\]}])/g, "$1");
+  return fixedString;
+}
+
 const entryPoints = existsSync(preloadPath)
-    ? { preload: preloadPath, app: appPath, }
-    : { app: appPath, };
+  ? { preload: preloadPath, app: appPath }
+  : { app: appPath };
 
 module.exports = {
-    entry: entryPoints,
-    target: "browserslist",
-    output: {
-        path: path.resolve("build"),
-        publicPath: "/",
+  entry: entryPoints,
+  target: "browserslist",
+  output: {
+    path: path.resolve("build"),
+    publicPath: "/",
+  },
+  resolve: {
+    fallback: {
+      dgram: false,
+      fs: false,
+      net: false,
+      tls: false,
+      child_process: false,
+      __dirname: false,
     },
-    resolve: {
-        fallback: {
-            dgram: false,
-            fs: false,
-            net: false,
-            tls: false,
-            child_process: false,
-            __dirname: false
-        },
-        modules: ["node_modules"],
-        extensions: [
-            ".web.ts", ".ts", ".web.tsx", ".tsx",
-            ".web.js", ".js", ".web.jsx", ".jsx",
-            ".json"
-        ]
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(worker)\.ts$/,
-                use: 'worker-loader',
-                exclude: /(node_modules|bower_components)/,
-            },
-            {
-                test: /\.(ts|js)x?$/,
-                exclude: /(node_modules|bower_components)/,
-                include: path.resolve("src"),
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        exclude: [
-                            /node_modules[\\\/]core-js/,
-                            /node_modules[\\\/]webpack[\\\/]buildin/,
-                        ],
-                        presets: [
-                            "@babel/preset-env",
-                            "@babel/preset-react",
-                            "@babel/preset-typescript",
-                        ],
-                        plugins: [
-                            "@babel/plugin-transform-runtime"
-                        ]
-                    }
-                }
-            },
-            {
-                test: /\.csv$/,
-                loader: 'csv-loader',
-                options: {
-                    dynamicTyping: true,
-                    header: true,
-                    skipEmptyLines: true
-                }
-            },
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                type: "asset/resource",
-            },
-            {
-                test: /\.(woff|woff2|ttf|eot|otf)$/i,
-                type: "asset/resource",
-                generator: {
-                    filename: `static/fonts/[name].${process.env["BUILD_HASH"]}[ext]`,
-                }
-            },
-            {
-                test: /\.svg$/,
-                type: 'asset/resource',
-                use: 'svgo-loader'
-            },
-        ]
-    },
-    plugins: [
-        new Dotenv(),
-        // new webpack.ProgressPlugin(),
-        new HtmlWebpackPlugin({
-            template: path.resolve("public", "index.html"),
-            minify: false,
-        }),
-        new InterpolateHtmlPlugin(
-            HtmlWebpackPlugin, { VALUE: null }
-        ),
-        new CopyWebpackPlugin({
-            patterns: [
-                {
-                    from: "./public",
-                    to: "./",
-                    globOptions: {
-                        ignore: [
-                            "**/*.html",
-                        ],
-                    },
-                }
+    modules: ["node_modules"],
+    extensions: [
+      ".web.ts",
+      ".ts",
+      ".web.tsx",
+      ".tsx",
+      ".web.js",
+      ".js",
+      ".web.jsx",
+      ".jsx",
+      ".json",
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(worker)\.ts$/,
+        use: "worker-loader",
+        exclude: /(node_modules|bower_components)/,
+      },
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /(node_modules|bower_components)/,
+        include: tsConfig ? getTsRootPaths(tsConfig) : path.resolve("src"),
+        use: {
+          loader: "babel-loader",
+          options: {
+            exclude: [
+              /node_modules[\\\/]core-js/,
+              /node_modules[\\\/]webpack[\\\/]buildin/,
             ],
-        }),
-        new webpack.IgnorePlugin({
-            resourceRegExp: /^\.\/locale$/,
-            contextRegExp: /moment$/,
-          }),
-        new WebpackManifestPlugin({
-            fileName: "asset-manifest.json"
-        }),
-    ]
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
+            plugins: ["@babel/plugin-transform-runtime"],
+          },
+        },
+      },
+      {
+        test: /\.csv$/,
+        loader: "csv-loader",
+        options: {
+          dynamicTyping: true,
+          header: true,
+          skipEmptyLines: true,
+        },
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|ttf|eot|otf)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: `static/fonts/[name].${process.env["BUILD_HASH"]}[ext]`,
+        },
+      },
+      {
+        test: /\.svg$/,
+        type: "asset/resource",
+        use: "svgo-loader",
+      },
+    ],
+  },
+  plugins: [
+    new Dotenv(),
+    // new webpack.ProgressPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve("public", "index.html"),
+      minify: false,
+    }),
+    new InterpolateHtmlPlugin(HtmlWebpackPlugin, { VALUE: null }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "./public",
+          to: "./",
+          globOptions: {
+            ignore: ["**/*.html"],
+          },
+        },
+      ],
+    }),
+    new webpack.IgnorePlugin({
+      resourceRegExp: /^\.\/locale$/,
+      contextRegExp: /moment$/,
+    }),
+    new WebpackManifestPlugin({
+      fileName: "asset-manifest.json",
+    }),
+  ],
 };
+
+function getTsRootPaths(config) {
+  const collectedPaths = [];
+  if (config && config.compilerOptions && config.compilerOptions.rootDirs) {
+    for (const rootDir of config.compilerOptions.rootDirs) {
+      collectedPaths.push(path.resolve(rootDir));
+    }
+  }
+  if (config && config.include && config.include) {
+    for (const includePath of config.include) {
+      collectedPaths.push(path.resolve(includePath));
+    }
+  }
+  const nonDuplicates = [...new Set(collectedPaths)];
+  return nonDuplicates;
+}
--- config/webpack.dev.server.js
+++ config/webpack.dev.server.js
@@ -1,14 +1,16 @@
 module.exports = {
-    compress: true,
-    hot: true,
-    liveReload: false,
-    open: (process.argv.includes("--silent") 
-    || process.argv.includes("-s")) ? false : true,
-    port: 3000,
-    client: {
-        overlay: true
-    },
-    setupExitSignals: true,
-    historyApiFallback: true,
-    watchFiles: ["src/**"]
-}
+  compress: true,
+  hot: true,
+  liveReload: false,
+  open:
+    process.argv.includes("--silent") || process.argv.includes("-s")
+      ? false
+      : true,
+  port: 3000,
+  client: {
+    overlay: true,
+  },
+  setupExitSignals: true,
+  historyApiFallback: true,
+  watchFiles: ["src/**"],
+};
